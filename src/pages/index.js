85d65e46270db13e7b7280e1e4db06926045ff6b
marcush12 <marcos012santos@gmail.com>
2020-03-15 20:25:20 -0300

    again

--- src/pages/index.js
+++ src/pages/index.js
@@ -43,6 +43,7 @@ export const query = graphql`
           category
           image {
             fixed(width: 50, height: 50) {
+              src
               ...GatsbyContentfulFixed_tracedSVG
             }
           }
